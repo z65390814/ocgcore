4021b88fece56ace8d56ba0ec000b9939e561adc
purerosefallen <78877@qq.com>
2018-10-04 23:59:03 +0800

    useless

--- interpreter.cpp
+++ interpreter.cpp
@@ -631,6 +631,7 @@ interpreter::interpreter(duel* pd): coroutines(256) {
 	set_duel_info(lua_state, pd);
 	//Initial
 	luaL_openlibs(lua_state);
+	/*
 	lua_pushnil(lua_state);
  	lua_setglobal(lua_state, "io");
  	lua_pushnil(lua_state);
@@ -641,6 +642,7 @@ interpreter::interpreter(duel* pd): coroutines(256) {
 	luaL_getsubtable(lua_state, LUA_REGISTRYINDEX, LUA_LOADED_TABLE);
 	lua_pushnil(lua_state);
 	lua_setfield(lua_state, -2, "os");
+	*/
 	//add bit lib back
 	lua_getglobal(lua_state, "bit32");
 	lua_setglobal(lua_state, "bit");
