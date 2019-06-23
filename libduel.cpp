9f7a67a34b0a345df2b98fd550765afb565c9107
purerosefallen <78877@qq.com>
2019-06-23 17:41:40 +0800

    fix

--- libduel.cpp
+++ libduel.cpp
@@ -128,8 +128,11 @@ int32 scriptlib::duel_exile(lua_State *L) {
 		pduel->game_field->send_to(pcard, pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, PLAYER_NONE, 0, 0, POS_FACEUP);
 	else
 		pduel->game_field->send_to(&(pgroup->container), pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, PLAYER_NONE, 0, 0, POS_FACEUP);
-	pduel->game_field->core.subunits.back().type = PROCESSOR_SENDTO;
-	return lua_yield(L, 0);
+	return lua_yieldk(L, 0, (lua_KContext)pduel, [](lua_State *L, int32 status, lua_KContext ctx) {
+		duel* pduel = (duel*)ctx;
+		lua_pushinteger(L, pduel->game_field->returns.ivalue[0]);
+		return 1;
+	});
 }
 int32 scriptlib::duel_disable_action_check(lua_State *L) {
 	check_param_count(L, 1);
