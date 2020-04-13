9d8eff03c99e62c04615bb7869e19d176008d841
mercury233 <me@mercury233.me>
2020-04-13 19:14:58 +0800

    build osx lua

--- premake4.lua
+++ premake4.lua
@@ -2,9 +2,11 @@ project "ocgcore"
     kind "StaticLib"
 
     files { "**.cc", "**.cpp", "**.c", "**.h" }
-    configuration { "windows", "macosx" }
+    configuration "windows"
         includedirs { "../lua" }
     configuration "not vs*"
         buildoptions { "-std=c++14" }
     configuration "linux"
         includedirs { "/usr/include/lua5.3" }
+    configuration "macosx" 
+        includedirs { "../lua" }
